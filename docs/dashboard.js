7c3ebf76d38e240a82b8952ef0a1194ef1c5bfb1
Markus Faessler <markus.faessler@abacus.ch>
2023-11-24 15:12:03 +0100

    feat: create/add user if clicked on save changes

--- docs/dashboard.js
+++ docs/dashboard.js
@@ -1,14 +1,51 @@
 const table = document.getElementById("customTable");
 const rows = table.getElementsByTagName("tr");
+const tableData = table.getElementsByTagName("tbody")[0];
 const firstName = document.getElementById("firstName");
 const lastName = document.getElementById("lastName");
 const address = document.getElementById("address");
 
+const cellClickListener = (event) => {
+  const cells = event.target.parentElement.getElementsByTagName("td");
+  firstName.innerText = `Firstname: ${cells[0].innerHTML}`;
+  lastName.innerText = `Lastname: ${cells[1].innerHTML}`;
+  address.innerText = `Address: ${cells[2].innerHTML}`;
+};
+
 Array.from(rows).forEach((row, index) => {
-  row.addEventListener("click", () => {
-    const cells = row.getElementsByTagName("td");
-    firstName.innerText = `Firstname: ${cells[0].innerHTML}`;
-    lastName.innerText = `Lastname: ${cells[1].innerHTML}`;
-    address.innerText = `Address: ${cells[2].innerHTML}`;
-  });
+  if (index !== 0) {
+    row.addEventListener("click", cellClickListener);
+  }
+});
+
+const addUserToTable = user => {
+  const row = document.createElement("tr");
+  row.classList.add("pointer");
+  row.setAttribute("data-bs-toggle", "offcanvas");
+  row.setAttribute("data-bs-target", "#offcanvasWithBackdrop");
+
+  const rowHead = document.createElement("th");
+  rowHead.setAttribute("scope", "row");
+  rowHead.innerText = Array.from(rows).length;
+  row.appendChild(rowHead);
+
+  var rowData = document.createElement("td");
+  rowData.innerText = user.firstName;
+  row.appendChild(rowData);
+  rowData = document.createElement("td");
+  rowData.innerText = user.lastName;
+  row.appendChild(rowData);
+  rowData = document.createElement("td");
+  rowData.innerText = user.address;
+  row.appendChild(rowData);
+
+  row.addEventListener("click", cellClickListener);
+
+  tableData.appendChild(row);
+};
+
+const saveUserBtn = document.getElementById("saveUser");
+saveUserBtn.addEventListener("click", event => {
+  console.log("save user clicked");
+  addUserToTable({ firstName: "Sup", lastName: "Blo", address: "Street 1" });
 });
