fcfd77ab502d25657d8184df6ef4ecd14ee4ec1e
Markus Faessler <markus.faessler@abacus.ch>
2023-11-24 08:11:50 +0100

    feat: get table content and set it on sidepanel

--- docs/dashboard.js
+++ docs/dashboard.js
@@ -1,10 +1,14 @@
 const table = document.getElementById("customTable");
 const rows = table.getElementsByTagName("tr");
-console.log(rows);
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const address = document.getElementById("address");
 
 Array.from(rows).forEach((row, index) => {
   row.addEventListener("click", () => {
     const cells = row.getElementsByTagName("td");
-    console.log(`Clicked on row ${index}`);
+    firstName.innerText = `Firstname: ${cells[0].innerHTML}`;
+    lastName.innerText = `Lastname: ${cells[1].innerHTML}`;
+    address.innerText = `Address: ${cells[2].innerHTML}`;
   });
 });
